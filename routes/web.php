00d47e3af230e8641630b3b5802ad55eac72c47e
flaviodematoshenrique <digitalclasse@gmail.com>
2023-01-16 11:18:58 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Login Routes:
+Route::get('/login', [UserController::class, 'loginView'])->name('login');
+Route::post('/signin', [UserController::class, 'loginPost']);
+
+// Register Routes:
+Route::get('/register', [UserController::class, 'registerView']);
+Route::post('/signup', [UserController::class, 'registerPost']);
+
+Route::get('/contacts/create', [ContactController::class, 'create']);
+
+// Route group:
+Route::group(['middleware' => 'auth'], function () {
+    // Get All Contacts Route:
+    Route::get('/contacts', [ContactController::class, 'getAll']);
+
+    // Routes for register new contacts on database:
+    Route::post('/contacts/store', [ContactController::class, 'store']);
+
+    // Routes for show contact by ID:
+    Route::get('/contacts/show/{id}', [ContactController::class, 'getById']);
+
+    // Edit Contacts Route:
+    Route::get('/contacts/edit/{id}', [ContactController::class, 'edit']);
+    Route::post('/contacts/update/{id}', [ContactController::class, 'update']);
+
+    // Destroy Contact Route:
+    Route::get('/contacts/delete/{id}', [ContactController::class, 'destroy']);
+});
